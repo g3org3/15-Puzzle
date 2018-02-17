47c9d4a7b4083d79299402d3bf0cbb1856917569
george <george@macbook-2.local>
2018-02-17 02:21:08 +0100

    update makefile

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | b
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install v7
+nvm use v7
 node -v
 # ------
 
